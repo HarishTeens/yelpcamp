19590dc3d0a6737200f9578326d9f0a40e3d3e7f
Harish <arishh2@gmail.com>
2019-07-26 21:25:54 +0530

    comment bug

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-var express       = require("express")(),
-    mongoose      = require("mongoose"),
-    bodyparser    = require("body-parser"),
-    campground    = require("./modules/campgrounds"),
-    comment       = require("./modules/comments"),
-    passport      = require("passport"),
-    User          = require("./modules/user"),
+var express = require("express")(),
+    mongoose = require("mongoose"),
+    bodyparser = require("body-parser"),
+    campground = require("./modules/campgrounds"),
+    comment = require("./modules/comments"),
+    passport = require("passport"),
+    User = require("./modules/user"),
     localstrategy = require("passport-local"),
-    passportlocalmongoose=require("passport-local-mongoose"),
-    methodOverride=require("method-override"),
-    flash         =require("connect-flash"),
-    cp=require("cookie-parser")
-    seedDB        =require("./seeds");
-    
-var commentRoutes   = require("./routes/comments"),
-    authRoutes      = require("./routes/auth"),
-    campgroundRoutes= require("./routes/campgrounds");
+    passportlocalmongoose = require("passport-local-mongoose"),
+    methodOverride = require("method-override"),
+    flash = require("connect-flash"),
+    cp = require("cookie-parser")
+seedDB = require("./seeds");
+
+var commentRoutes = require("./routes/comments"),
+    authRoutes = require("./routes/auth"),
+    campgroundRoutes = require("./routes/campgrounds");
 
 //seedDB();
 express.use(cp());
-express.set("view engine","ejs");
-express.use(bodyparser.urlencoded({extended:true}));
+express.set("view engine", "ejs");
+express.use(bodyparser.urlencoded({ extended: true }));
+
 mongoose.Promise = global.Promise;
-//
-mongoose.connect("mongodb://harish:harish@ds131697.mlab.com:31697/yelpcamp",{useMongoClient:true})
+mongoose.connect("mongodb://harish:harish16@ds131697.mlab.com:31697/yelpcamp", { useMongoClient: true })
+    //mongoose.connect("mongodb://localhost/yelpcamp", { useMongoClient: true });
 express.use(require("express").static("public"));
 express.use(methodOverride("_method"));
 express.use(flash());
 
 //PASSPORT CONFIGURATIONS
 express.use(require("express-session")({
-    secret:"All You want to do is stay a minute ",
-    resave:false,
-    saveUninitialized:false,
-     cookie: {
-        maxAge:60*1000
-        }
-        
-    
+    secret: "All You want to do is stay a minute ",
+    resave: false,
+    saveUninitialized: false,
 }));
 express.use(passport.initialize());
 express.use(passport.session());
@@ -44,18 +40,18 @@ passport.use(new localstrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-express.use(function(req,res,next){
-  res.locals.success=req.flash("success");
-  res.locals.error=req.flash("error");
-  res.locals.currentUser=req.user;  
-  next();
+express.use(function(req, res, next) {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currentUser = req.user;
+    next();
 })
 
 //requiring routes
-express.use("/campgrounds/:id/comments",commentRoutes);
+express.use("/campgrounds/:id/comments", commentRoutes);
 express.use(authRoutes);
-express.use("/campgrounds",campgroundRoutes);
+express.use("/campgrounds", campgroundRoutes);
 
-express.listen(process.env.PORT,process.env.IP,function(){
+express.listen(3000, function() {
     console.log("Started YelpCamp server....");
 })
